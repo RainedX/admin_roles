76da60ecc69dc08919fd2f02166f05082079a1cd
RainedX <1355247497@qq.com>
2019-08-10 10:01:09 +0800

    next(...to)报错

--- src/permission.js
+++ src/permission.js
@@ -1,58 +1,61 @@
-import router from './router'
-import store from './store'
-import { Message } from 'element-ui'
-import NProgress from 'nprogress'
-import 'nprogress/nprogress.css'
-import { getToken } from '@/utils/auth'
-import setPageTitle from '@/utils/setPageTitle'
+import router from "./router";
+import store from "./store";
+import { Message } from "element-ui";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+import { getToken } from "@/utils/auth";
+import setPageTitle from "@/utils/setPageTitle";
 
 // not show circle
-NProgress.configure({ showSpinner: false })
+NProgress.configure({ showSpinner: false });
 
-const whiteList = ['/login']
+const whiteList = ["/login"];
 
 router.beforeEach(async (to, from, next) => {
-    NProgress.start()
+  NProgress.start();
 
-    document.title = setPageTitle(to.meta.title)
+  document.title = setPageTitle(to.meta.title);
 
-    const hasToken = getToken()
+  const hasToken = getToken();
 
-    if (hasToken) {
-        if (to.path === '/login') {
-            // 跳转{path: '/'}时重定向到'/home',又触发一次beforeEach
-            next({ path: '/' })
-            NProgress.done()
-        } else {
-            const hasRoles = store.getters.roles && store.getters.roles.length > 0
-            // 初始化时，角色为空
-            if (hasRoles) {
-                next()
-            } else {
-                try {
-                    const { roles } = await store.dispatch('user/getUserInfo')
-                    const accessRoutes = await store.dispatch('permission/generateRoutes', roles)
-
-                    router.addRoutes(accessRoutes)
-                    next({ ...to, replace: true })
-                } catch (error) {
-                    await store.dispatch('user/resetToken')
-                    Message.error(error || 'Has Error')
-                    next(`/login?redirect=${to.path}`)
-                    NProgress.done()
-                }
-            }
-        }
+  if (hasToken) {
+    if (to.path === "/login") {
+      // 跳转{path: '/'}时重定向到'/home',又触发一次beforeEach
+      next({ path: "/" });
+      NProgress.done();
     } else {
-        if (whiteList.indexOf(to.path) !== -1) {
-            next()
-        } else {
-            next(`login?redirect=${to.path}`)
-            NProgress.done()
+      const hasRoles = store.getters.roles && store.getters.roles.length > 0;
+      // 初始化时，角色为空
+      if (hasRoles) {
+        next();
+      } else {
+        try {
+          const { roles } = await store.dispatch("user/getUserInfo");
+          const accessRoutes = await store.dispatch(
+            "permission/generateRoutes",
+            roles
+          );
+
+          router.addRoutes(accessRoutes);
+          next({ ...to, replace: true });
+        } catch (error) {
+          await store.dispatch("user/resetToken");
+          Message.error(error || "Has Error");
+          next(`/login?redirect=${to.path}`);
+          NProgress.done();
         }
+      }
     }
-})
+  } else {
+    if (whiteList.indexOf(to.path) !== -1) {
+      next();
+    } else {
+      next(`login?redirect=${to.path}`);
+      NProgress.done();
+    }
+  }
+});
 
 router.afterEach(() => {
-    NProgress.done()
-})
+  NProgress.done();
+});
